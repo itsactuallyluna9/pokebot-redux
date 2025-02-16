3795fa7f45bb47f58e958e85164b0f7b0202f1d4
Luna <itsactuallyluna9@gmail.com>
2025-02-16 16:49:03 -0600

    make output fancy using embeds

--- c4_discord_bot/cogs/pokeCog/pokeCog.py
+++ c4_discord_bot/cogs/pokeCog/pokeCog.py
@@ -1,26 +1,33 @@
-from discord import app_commands, Interaction
+from discord import app_commands, Interaction, Embed
 from discord.ext import commands
 from typing import Optional
-import json
-
-from .pokebot_evolved import PokebotEvolved
 
 class PokeCog(commands.Cog):
     
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.state = None
     
     @app_commands.command()
     @app_commands.describe(name='Enter a name to generate information for (optional)')
     async def generate(self, interaction: Interaction, name: Optional[str]=None):
-        #entry = await PokebotEvolved.generateFullEntry(prompt=name)
-        entry = PokebotEvolved().nameTypeStat(prompt=name)
-        new_entry = list()
-        new_entry.append(entry[0])
-        new_entry.append(entry[1])
-        new_entry.append(list(int(x) for x in entry[2]))
-        out = json.dumps(new_entry)
-        await interaction.response.send_message(out)
+        if not self.state:
+            # lazy load
+            from .pokebot_evolved import PokebotEvolved
+            self.state = PokebotEvolved()
+        
+        entry = self.state.generateFullEntry(prompt=name, print_to_console=False)
+        embed = self.make_embed(entry)
+        await interaction.response.send_message(embed=embed)
+    
+    def make_embed(self, entry: dict):
+        embed = Embed(title=entry['name'], description=entry['description'])
+        if entry['category'] is not None:
+            embed.add_field(name='Category', value=entry['category'])
+        embed.add_field(name='Type', value=entry['type1'] + (', ' + entry['type2'] if entry['type2'] else ''))
+        embed.add_field(name='Stats', value=f"HP: {entry['hp']}\nAttack: {entry['atk']}\nDefense: {entry['def']}\nSpecial Attack: {entry['spatk']}\nSpecial Defense: {entry['spdef']}\nSpeed: {entry['speed']}")
+        embed.set_author(name='Generated using Pokebot Evolved')
+        return embed
 
 async def setup(bot):
     await bot.add_cog(PokeCog(bot))
